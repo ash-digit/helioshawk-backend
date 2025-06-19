0a61d558eefb42b800c6bf9a1dfe7a938beacdcb
ash-digit <ashdigit44@gmail.com>
2025-06-19 21:48:51 +0100

    Initial commit

---
+++ src/main/java/com/example/HeliosHawkDev/HeliosHawkDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.HeliosHawkDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliosHawkDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliosHawkDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HeliosHawkDev/HeliosHawkDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HeliosHawkDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliosHawkDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
